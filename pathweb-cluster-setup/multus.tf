58a82e97709ad9642f4d043ae4822261b8f4bf91
Yip Rui Fung <rf@yrf.me>
2022-12-27 14:06:19 +0800

    Remove multus from terraform

--- pathweb-cluster-setup/multus.tf
+++
@@ -1,8 +0,0 @@
-data "kubectl_file_documents" "multus" {
-  content = file("${path.module}/multus-daemonset.yaml")
-}
-
-resource "kubectl_manifest" "multus" {
-  for_each  = data.kubectl_file_documents.multus.manifests
-  yaml_body = each.value
-}
