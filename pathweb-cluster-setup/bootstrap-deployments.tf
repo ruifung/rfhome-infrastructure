b7db9c890c789bf797b5dbc2b05e1fa0a5d7b79f
Yip Rui Fung <rf@yrf.me>
2022-12-27 16:34:36 +0800

    Bootstrap some things before handing control over to flux

---
+++ pathweb-cluster-setup/bootstrap-deployments.tf
@@ -0,0 +1,26 @@
+resource "helm_release" "sealed_secrets" {
+  name       = "sealed-secrets"
+  repository = "https://bitnami-labs.github.io/sealed-secrets"
+  chart      = "sealed-secrets"
+  namespace  = "kube-system"
+}
+
+data "kubectl_file_documents" "multus" {
+  content = file("${path.module}/multus-cni.yaml")
+}
+
+resource "kubectl_manifest" "multus" {
+  for_each  = data.kubectl_file_documents.multus.manifests
+  yaml_body = each.value
+}
+
+resource "helm_release" "tf_controller" {
+  name       = "tf-controller"
+  repository = "https://weaveworks.github.io/tf-controller/"
+  chart      = "tf-controller"
+  namespace  = "kube-system"
+
+  values = [yamlencode({
+    replicaCount = 0
+  })]
+}
