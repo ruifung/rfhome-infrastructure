f00f0462b11d5f43a5cf841cae4af0f40c3506ba
Yip Rui Fung <rf@yrf.me>
2023-02-02 00:01:56 +0800

    nodered: move extra-npm-modules script to configmap

---
+++ deployments/pathweb/node-red/extra-npm-modules.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /data
+function install_package_if_needed() {
+    local pkg=${1:-Package required}
+    local version=${2:-Version required}
+    local installTarget="$p@$v"
+    shift 2
+    if [ "$3" =~ "^git+" ]; then
+        installTarget=${3:-InstallURL}
+        shift 1
+    fi
+    local i=$(node -p "require('$pkg/package.json').version" 2>/dev/null)
+    [ "$i" == "$version" ] || npm "$@" install $installTarget
+}
+install_package_if_needed node-red-context-redis 0.0.1 git+https://github.com/node-red/
+install_package_if_needed passport-oauth2 1.6.1
