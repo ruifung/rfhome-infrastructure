019c67295fcd19c005a9e5761226e8bd47a3522c
Yip Rui Fung <rf@yrf.me>
2023-02-02 00:52:40 +0800

    nodered: attempt to fix extra-tools setup

---
+++ deployments/pathweb/node-red/extra-tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd $(dirname $0)
+if [[ ! -f speedtest/speedtest-$SPEEDTEST_VERSION ]]; then
+    rm -rf speedtest
+    mkdir -p speedtest
+    pushd speedtest 
+    curl https://install.speedtest.net/app/cli/ookla-speedtest-$SPEEDTEST_VERSION-linux-$(arch).tgz | tar xvz
+    touch speedtest-$SPEEDTEST_VERSION
+    popd
+fi
