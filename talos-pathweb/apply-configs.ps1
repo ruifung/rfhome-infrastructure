45dea08874cc645a1f9656814a803bf7c7bdebef
Yip Rui Fung <rf@yrf.me>
2023-02-25 22:55:31 +0800

    Go back to previous subnet because it broke stuff

--- talos-pathweb/apply-configs.ps1
+++ talos-pathweb/apply-configs.ps1
@@ -1,12 +1,13 @@
 $mode, $extraArgs = $args
 $extraArgs = $extraArgs -join " "
+$dnsSuffix = "pathweb.clusters.home.yrf.me"
 Write-Output "Extra Args: $extraArgs"
 if ($extraArgs -eq "") { $extraArgs = "--mode=auto" }
 if (($mode -eq "controlplane") -or ($mode -eq "all")) {
     Write-Output "Applying control-plane machine configs."
-    talosctl apply-config --talosconfig .\talosconfig --nodes 10.229.97.31 --file .\base\controlplane.yaml -p @patches\node-patches.yaml,@patches\controlplane-patches.yaml,@patches\control-1.yaml $extraArgs
-    talosctl apply-config --talosconfig .\talosconfig --nodes 10.229.97.32 --file .\base\controlplane.yaml -p @patches\node-patches.yaml,@patches\controlplane-patches.yaml,@patches\control-2.yaml $extraArgs
-    talosctl apply-config --talosconfig .\talosconfig --nodes 10.229.97.33 --file .\base\controlplane.yaml -p @patches\node-patches.yaml,@patches\controlplane-patches.yaml,@patches\control-3.yaml $extraArgs
+    talosctl apply-config --talosconfig .\talosconfig --nodes 1.controlplane.$dnsSuffix --file .\base\controlplane.yaml -p @patches\node-patches.yaml,@patches\controlplane-patches.yaml,@patches\control-1.yaml $extraArgs
+    talosctl apply-config --talosconfig .\talosconfig --nodes 2.controlplane.$dnsSuffix --file .\base\controlplane.yaml -p @patches\node-patches.yaml,@patches\controlplane-patches.yaml,@patches\control-2.yaml $extraArgs
+    talosctl apply-config --talosconfig .\talosconfig --nodes 3.controlplane.$dnsSuffix --file .\base\controlplane.yaml -p @patches\node-patches.yaml,@patches\controlplane-patches.yaml,@patches\control-3.yaml $extraArgs
 }
 if (($mode -eq "worker") -or ($mode -eq "all")) {
     Write-Output "Applying worker-nodes machine configs."
