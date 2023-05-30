90d366cba12c88aab6ebe526420602b9c522ec46
Yip Rui Fung <rf@yrf.me>
2023-05-30 16:17:56 +0800

    Update talos and kubernetes.

--- talos-pathweb/generate-configs.ps1
+++ talos-pathweb/generate-configs.ps1
@@ -1,6 +1,6 @@
 Push-Location base
-$KUBE_VERSION=1.26.1
-$TALOS_VERSION=1.4.4
+$KUBE_VERSION="1.26.5"
+$TALOS_VERSION="v1.4.4"
 talosctl gen config pathweb "https://10.229.17.1:6443" --output-dir _out --with-secrets secrets.yaml --kubernetes-version $KUBE_VERSION --talos-version $TALOS_VERSION
 talosctl --talosconfig .\_out\talosconfig config endpoint 10.229.17.1
 talosctl --talosconfig .\_out\talosconfig config endpoint 10.229.17.1 10.229.17.2 10.229.17.3
