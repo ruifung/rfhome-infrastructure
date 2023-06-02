54f876467fcec1bfc8b0d775b325f28c369acefd
Yip Rui Fung <rf@yrf.me>
2023-06-03 01:35:34 +0800

    Update apply-configs script.

--- talos-pathweb/apply-configs.ps1
+++ talos-pathweb/apply-configs.ps1
@@ -1,18 +1,67 @@
 $mode, $extraArgs = $args
 $extraArgs = $extraArgs -join " "
-$dnsSuffix = "pathweb.clusters.home.yrf.me"
+$homeDnsSuffix = "pathweb.clusters.home.yrf.me"
+$controlplane = @(
+    @{
+        fqdn = "1.controlplane.$homeDnsSuffix";
+        base = '.\base\controlplane.yaml';
+        patches = @('@patches\node-patches.yaml','@patches\controlplane-patches.yaml','@patches\control-1.yaml')
+    },
+    @{
+        fqdn = "2.controlplane.$homeDnsSuffix";
+        base = '.\base\controlplane.yaml';
+        patches = @('@patches\node-patches.yaml','@patches\controlplane-patches.yaml','@patches\control-2.yaml')
+    },
+    @{
+        fqdn = "3.controlplane.$homeDnsSuffix";
+        base = '.\base\controlplane.yaml';
+        patches = @('@patches\node-patches.yaml','@patches\controlplane-patches.yaml','@patches\control-3.yaml')
+    }
+)
+$workers = @(
+    @{
+        fqdn = "1.workers.$homeDnsSuffix";
+        base = '.\base\worker.yaml';
+        patches = @('@patches\node-patches.yaml','@patches\worker-patches.yaml','@patches\worker-1.yaml')
+    },
+    @{
+        fqdn = "2.workers.$homeDnsSuffix";
+        base = '.\base\worker.yaml';
+        patches = @('@patches\node-patches.yaml','@patches\worker-patches.yaml','@patches\worker-2.yaml')
+    },
+    @{
+        fqdn = "3.workers.$homeDnsSuffix";
+        base = '.\base\worker.yaml';
+        patches = @('@patches\node-patches.yaml','@patches\worker-patches.yaml','@patches\worker-3.yaml')
+    },
+    @{
+        fqdn = 'pathweb-piworker-1.vsvc.home.arpa';
+        base = '.\base\worker.yaml';
+        patches = @('@patches\node-patches.yaml','@patches\pi-worker.yaml','@patches\piworker-1.yaml')
+    }
+)
+$cloudworkers = @(
+    @{
+        fqdn = 'cloud-1.pathweb.0spkl.dev';
+        base = '.\base\worker.yaml';
+        patches = @('@patches\cloud-node-patches.yaml','@vultr-cloud\vultr-worker.yaml','@vultr-cloud\cloud-1.yaml')
+    }
+)
+
 Write-Output "Extra Args: $extraArgs"
 if ($extraArgs -eq "") { $extraArgs = "--mode=auto" }
+$toApply = @()
 if (($mode -eq "controlplane") -or ($mode -eq "all")) {
-    Write-Output "Applying control-plane machine configs."
-    talosctl apply-config --talosconfig .\talosconfig --nodes 1.controlplane.$dnsSuffix --file .\base\controlplane.yaml -p @patches\node-patches.yaml,@patches\controlplane-patches.yaml,@patches\control-1.yaml $extraArgs
-    talosctl apply-config --talosconfig .\talosconfig --nodes 2.controlplane.$dnsSuffix --file .\base\controlplane.yaml -p @patches\node-patches.yaml,@patches\controlplane-patches.yaml,@patches\control-2.yaml $extraArgs
-    talosctl apply-config --talosconfig .\talosconfig --nodes 3.controlplane.$dnsSuffix --file .\base\controlplane.yaml -p @patches\node-patches.yaml,@patches\controlplane-patches.yaml,@patches\control-3.yaml $extraArgs
+    $toApply = $toApply + $controlplane
 }
-if (($mode -eq "worker") -or ($mode -eq "all")) {
-    Write-Output "Applying worker-nodes machine configs."
-    talosctl apply-config --talosconfig .\talosconfig --nodes 1.workers.$dnsSuffix --file .\base\worker.yaml -p @patches\node-patches.yaml,@patches\worker-patches.yaml,@patches\worker-1.yaml $extraArgs
-    talosctl apply-config --talosconfig .\talosconfig --nodes 2.workers.$dnsSuffix --file .\base\worker.yaml -p @patches\node-patches.yaml,@patches\worker-patches.yaml,@patches\worker-2.yaml $extraArgs
-    talosctl apply-config --talosconfig .\talosconfig --nodes 3.workers.$dnsSuffix --file .\base\worker.yaml -p @patches\node-patches.yaml,@patches\worker-patches.yaml,@patches\worker-3.yaml $extraArgs
-    talosctl apply-config --talosconfig .\talosconfig --nodes pathweb-piworker-1.vsvc.home.arpa --file .\base\worker.yaml -p @patches\node-patches.yaml,@patches\pi-worker.yaml,@patches\piworker-1.yaml $extraArgs
+if (($mode -eq "workers") -or ($mode -eq "all")) {
+    $toApply = $toApply + $workers
+}
+if (($mode -eq "cloud") -or ($mode -eq "all")) {
+    $toApply = $toApply + $cloudworkers
+}
+
+foreach ($node in $toApply) {
+    Write-Output "Applying configuration for node: $($node.fqdn)"
+    talosctl apply-config --talosconfig .\talosconfig --nodes $($node.fqdn) --file $($node.base) -p $($node.patches -join ',') $extraArgs
 }
