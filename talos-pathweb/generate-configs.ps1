bf7c8fdf4b8e3fe045a3e03292ea3a905efc7c2d
Yip Rui Fung <rf@yrf.me>
2023-09-27 00:33:51 +0800

    Update talos, cilium, and re-enable kubeprism

--- talos-pathweb/generate-configs.ps1
+++ talos-pathweb/generate-configs.ps1
@@ -1,6 +1,6 @@
 Push-Location base
 $KUBE_VERSION="1.27.5"
-$TALOS_VERSION="v1.5.1"
+$TALOS_VERSION="v1.5.3"
 talosctl gen config pathweb "https://controlplane.pathweb.clusters.home.yrf.me:6443" --output-dir _out --with-secrets secrets.yaml --with-docs=false --with-examples=false --with-kubespan --kubernetes-version $KUBE_VERSION --talos-version $TALOS_VERSION
 talosctl --talosconfig .\_out\talosconfig config endpoint controlplane.pathweb.clusters.home.yrf.me
 talosctl --talosconfig .\_out\talosconfig config node pathweb-control-1.servers.home.yrf.me pathweb-control-2.servers.home.yrf.me pathweb-control-3.servers.home.yrf.me pathweb-worker-1.servers.home.yrf.me pathweb-worker-2.servers.home.yrf.me pathweb-worker-3.servers.home.yrf.me
