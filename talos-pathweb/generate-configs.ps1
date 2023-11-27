f50fd0eb983e549242c367c6ac41d7466d3cc5b7
Yip Rui Fung <rf@yrf.me>
2023-11-28 00:37:06 +0800

    talos: enable ipv6

--- talos-pathweb/generate-configs.ps1
+++ talos-pathweb/generate-configs.ps1
@@ -1,7 +1,7 @@
 Push-Location base
 $KUBE_VERSION="1.28.3"
 $TALOS_VERSION="v1.5.5"
-$TALOS_FACTORY_SCHEMATIC_ID="8e8827b5b91420728f8415f3dc200fbf23b425ec07bf27cdc92a676367ee9edf"
+$TALOS_FACTORY_SCHEMATIC_ID="73aca3ec1ea78b5455244413f6f01e0eb3e17a18501dddf0a6ef8074ce5bcd04"
 # $TALOS_INSTALL_IMAGE="factory.talos.dev/installer/${TALOS_FACTORY_SCHEMATIC_ID}:${TALOS_VERSION}"
 $TALOS_INSTALL_IMAGE="harbor.services.home.yrf.me/talos-image-factory/installer/${TALOS_FACTORY_SCHEMATIC_ID}:${TALOS_VERSION}"
 talosctl gen config pathweb "https://controlplane.pathweb.clusters.home.yrf.me:6443" --output-dir _out --with-secrets secrets.yaml --with-docs=false --with-examples=false --with-kubespan --kubernetes-version $KUBE_VERSION --talos-version $TALOS_VERSION --install-image $TALOS_INSTALL_IMAGE
