cfa2d8234868abab751025dbea43b2655b24ef81
Yip Rui Fung <rf@yrf.me>
2024-05-04 00:29:26 +0800

    update talos config.

--- talos-pathweb/generate-configs.ps1
+++ talos-pathweb/generate-configs.ps1
@@ -9,7 +9,7 @@ $TALOS_FACTORY_SCHEMATIC_ID="71fdc73baac9ae32e13435992ba56d469b4eaf3fb561125e1af
 $TALOS_INSTALL_IMAGE="factory.talos.dev/installer/${TALOS_FACTORY_SCHEMATIC_ID}:${TALOS_VERSION}"
 talosctl gen config pathweb "https://controlplane.pathweb.clusters.home.yrf.me:6443" --output-dir _out --with-secrets secrets.yaml --with-docs=false --with-examples=false --with-kubespan --kubernetes-version $KUBE_VERSION --talos-version $TALOS_VERSION --install-image $TALOS_INSTALL_IMAGE
 talosctl --talosconfig .\_out\talosconfig config endpoint controlplane.pathweb.clusters.home.yrf.me
-talosctl --talosconfig .\_out\talosconfig config node pathweb-control-1.servers.home.yrf.me pathweb-control-2.servers.home.yrf.me pathweb-control-3.servers.home.yrf.me pathweb-worker-1.servers.home.yrf.me pathweb-worker-2.servers.home.yrf.me pathweb-worker-3.servers.home.yrf.me
+talosctl --talosconfig .\_out\talosconfig config node pathweb-control-1.servers.home.yrf.me pathweb-control-2.servers.home.yrf.me pathweb-control-3.servers.home.yrf.me pathweb-worker-1.servers.home.yrf.me pathweb-worker-2.servers.home.yrf.me pathweb-worker-3.servers.home.yrf.me pathweb-worker-baldric.servers.home.yrf.me
 
 Move-Item _out\talosconfig ..\talosconfig -Force
 Move-Item _out\controlplane.yaml controlplane.yaml -Force
