833f2a9073b9e8dd247b51e699e85e8401c02a1e
Yip Rui Fung <rf@yrf.me>
2024-05-09 09:59:52 +0800

    Update talos config to enable watchdog functionality.

--- talos-pathweb/apply-configs.ps1
+++ talos-pathweb/apply-configs.ps1
@@ -1,73 +1,42 @@
+$ErrorActionPreference = "Stop"
+
 $mode, $extraArgs = $args
 $extraArgs = $extraArgs -join " "
 $homeDnsSuffix = "servers.home.yrf.me"
-$controlplane = @(
-    @{
-        fqdn = "pathweb-control-1.$homeDnsSuffix";
-        base = '.\base\controlplane.yaml';
-        patches = @('@patches\node-patches.yaml','@patches\controlplane-firewall.yaml','@patches\controlplane-patches.yaml','@patches\control-1.yaml', '@patches\harbor-rfhome.secret.yaml')
-    },
-    @{
-        fqdn = "pathweb-control-2.$homeDnsSuffix";
-        base = '.\base\controlplane.yaml';
-        patches = @('@patches\node-patches.yaml','@patches\controlplane-firewall.yaml','@patches\controlplane-patches.yaml','@patches\control-2.yaml', '@patches\harbor-rfhome.secret.yaml')
-    },
-    @{
-        fqdn = "pathweb-control-3.$homeDnsSuffix";
-        base = '.\base\controlplane.yaml';
-        patches = @('@patches\node-patches.yaml','@patches\controlplane-firewall.yaml','@patches\controlplane-patches.yaml','@patches\control-3.yaml', '@patches\harbor-rfhome.secret.yaml')
-    }
-)
-$workers = @(
-    @{
-        fqdn = "pathweb-worker-1.$homeDnsSuffix";
-        base = '.\base\worker.yaml';
-        patches = @('@patches\node-patches.yaml','@patches\worker-patches.yaml','@patches\worker-1.yaml', '@patches\harbor-rfhome.secret.yaml')
-    },
-    @{
-        fqdn = "pathweb-worker-2.$homeDnsSuffix";
-        base = '.\base\worker.yaml';
-        patches = @('@patches\node-patches.yaml','@patches\worker-patches.yaml','@patches\worker-2.yaml', '@patches\harbor-rfhome.secret.yaml')
-    },
-    @{
-        fqdn = "pathweb-worker-3.$homeDnsSuffix";
-        base = '.\base\worker.yaml';
-        patches = @('@patches\node-patches.yaml','@patches\worker-patches.yaml','@patches\worker-3.yaml', '@patches\harbor-rfhome.secret.yaml')
-    }
-    @{
-        fqdn = "pathweb-worker-baldric.$homeDnsSuffix";
-        base = '.\base\worker.yaml';
-        patches = @('@patches\node-patches.yaml','@patches\worker-patches.yaml','@patches\worker-baldric.yaml', '@patches\harbor-rfhome.secret.yaml')
-    }
-    @{
-        fqdn = "pathweb-worker-pi4-01.$homeDnsSuffix";
-        base = '.\base\worker.yaml';
-        patches = @('@patches\node-patches.yaml','@patches\worker-pi4-patches.yaml','@patches\worker-pi4-01.yaml', '@patches\harbor-rfhome.secret.yaml')
-    }
-)
 
+$nodes = Get-Content nodes.json -Raw | ConvertFrom-Json | Where-Object {$_.ignore -ne $true}
+
+if ($null -eq $mode) {
+    Write-Error "No mode specified. Specify either a node or one of the following: controlplane, workers, all"
+    Exit
+}
+
+if (-not ($extraArgs -like "*--mode*")) { $extraArgs = "--mode try $extraArgs"}
 Write-Output "Extra Args: $extraArgs"
-if ($extraArgs -eq "") { $extraArgs = "--mode=auto" }
 $toApply = @()
-if (($mode -eq "controlplane") -or ($mode -eq "all")) {
-    $toApply = $toApply + $controlplane
-}
+# Apply the workers first and controlplane last.
 if (($mode -eq "workers") -or ($mode -eq "all")) {
-    $toApply = $toApply + $workers
+    $toApply = $toApply + ($nodes | Where-Object {$_.role -eq "worker"})
 }
+if (($mode -eq "controlplane") -or ($mode -eq "all")) {
+    $toApply = $toApply + ($nodes | Where-Object {$_.role -eq "controlplane"})
+}
+
 #if toApply is empty, split mode by comma and append result to toApply after trimming excess whitespace
 if ($toApply.Count -eq 0) {
-    $targets = $mode.Split(',')
-    $toApply = $controlplane + $workers
+    $targets = $mode.Split(',') | Where-Object { $_ -ne "" }
     # filter toApply by fqdn in $targets
-    $toApply = $toApply | Where-Object { ($targets -contains $_.fqdn) -or ($targets -contains ($_.fqdn -replace "\.$homeDnsSuffix","")) } 
+    $toApply = $nodes | Where-Object { 
+        $node = $_
+        ($targets -contains $node.fqdn) -or (($targets | Where-Object { "$_.$homeDnsSuffix" -eq $node.fqdn }).Count -gt 0)
+    } 
 }
 # if toApply is still empty, print out all available fqdns
 if ($toApply.Count -eq 0) {
     Write-Output "No nodes found for mode [$mode]"
     Write-Output "Available nodes:"
-    Write-Output "controlplane: $($controlplane.fqdn -join ', ')"
-    Write-Output "workers: $($workers.fqdn -join ', ')"
+    Write-Output "controlplane: $(($nodes | Where-Object {$_.role -eq "controlplane"}).fqdn -join ', ')"
+    Write-Output "workers: $(($nodes | Where-Object {$_.role -eq "worker"}).fqdn -join ', ')"
     exit 1
 }
 
@@ -76,14 +45,25 @@ foreach ($node in $toApply) {
     $bootstrapIpPattern = "--bootstrap-ip=([a-fA-F0-9:.]+)+"
     $targetNode = $node.fqdn
     $bootstrapIp = ""
+
     if ($extraArgs -contains "--insecure" -and $extraArgs -match $bootstrapIpPattern) {
         $bootstrapIp = ($extraArgs | Select-String -Pattern $bootstrapIpPattern).Matches.Groups[1].Value
         $extraArgs = $extraArgs -replace $bootstrapIpPattern,"-e $bootstrapIp"
         $targetNode = $bootstrapIp
-        Write-Output "Bootstraping node at address: $bootstrapIp"
+        Write-Output "Uploading initial config to node at address: $bootstrapIp"
     }
-    $extraArgs = $extraArgs -split " "
+
+
+    $machineConfigFile = ".\machineconfig\$($node.fqdn).yaml"
+
+    $extraArgs = $extraArgs -split " " | Where-Object { $_ -ne "" }
     if ($targetNode -ne "") {
-        talosctl apply-config --talosconfig .\talosconfig --nodes $($targetNode) --file $($node.base) -p $($node.patches -join ',') @extraArgs
+        Write-Output "> talosctl apply-config --talosconfig .\talosconfig --nodes $($targetNode) --file $($machineConfigFile) $extraArgs"
+        talosctl apply-config --talosconfig .\talosconfig --nodes $($targetNode) --file $($machineConfigFile) @extraArgs
+        if ($LASTEXITCODE -ne 0) {
+            Write-Error "talosctl exited with exit code $LASTEXITCODE"
+            Exit
+        }
     }
+    Write-Output ""
 }
